06b93ab853040cf231a18a6c23277d28a257c5c2
Timothy Gnanaseelan <71440878+timothy777-ux@users.noreply.github.com>
2026-03-11 10:02:10 +0530

    redo

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,17 +1,5 @@
 public class HelloApp{
-
-    public static void main(String[] args) {
-		String name="Timothy";
-
-       
-        if (args.length == 0) {
-           
-
-       
-         name = args[0];
-		}
-
-      
-        System.out.println("Hello, " + name + "!!");
-    }
+public static void main(String args[]){
+System.out.println("Hello world!");
+}
 }
