bdf2c8c2462bc8efebc6c41ee0bcb138097c99a0
Timothy Gnanaseelan <71440878+timothy777-ux@users.noreply.github.com>
2026-03-18 08:33:59 +0530

    new feature ig

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,5 +1,18 @@
 public class HelloApp{
-public static void main(String args[]){
-System.out.println("Hello world");
+
+    public static void main(String[] args) {
+
+        String name;
+
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "Shashank ";
+        }
+
+        System.out.println("Hello, " + name + "!");
+
+    }
+
 }
 }
