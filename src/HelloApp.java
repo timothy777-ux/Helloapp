50eee71f1626a4abc66a98308e765f33d2eef6c8
Timothy Gnanaseelan <71440878+timothy777-ux@users.noreply.github.com>
2026-03-18 09:42:17 +0530

    Change default name from 'Shashank' to 'TIMOTHY'

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -7,7 +7,7 @@ public class HelloApp{
         if (args.length > 0) {
             name = args[0];
         } else {
-            name = "Shashank ";
+            name = "TIMOTHY ";
         }
 
         System.out.println("Hello, " + name + "!");
