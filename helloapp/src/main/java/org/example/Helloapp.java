c0f0a442f21ceb9e65adc19c812e363eaa63dc2b
Timothy Gnanaseelan <71440878+timothy777-ux@users.noreply.github.com>
2026-03-26 07:52:49 +0530

    new feature

---
+++ helloapp/src/main/java/org/example/Helloapp.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Helloapp {
+    public static void main(String[] args) {
+
+        // Check if command-line arguments are provided
+        if (args.length > 0) {
+            System.out.print("Hello ");
+
+            // Print all arguments
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i] + " ");
+            }
+        } else {
+            // Default message if no arguments
+            System.out.println("Hello World");
+        }
+    }
+}
